d5d1e58c5ec967898c5ea1d20059202537520413
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-06 17:45:08 +0500

    fix

--- main.py
+++ main.py
@@ -1,78 +1,78 @@
-import asyncio
+# import asyncio
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
-from aiogram.types import (
-    ReplyKeyboardMarkup,
-    KeyboardButton,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-    ContentTypes,
-)
+# from aiogram import Bot, Dispatcher, types, F
+# from aiogram.filters import Command
+# from aiogram.types import (
+#     ReplyKeyboardMarkup,
+#     KeyboardButton,
+#     InlineKeyboardMarkup,
+#     InlineKeyboardButton,
+#     ContentType,
+# )
 
-# Define your bot token and create Bot and Dispatcher instances
-bot_token = "7293607513:AAEMbszahoLrfxC5_28fEojq0AGCkKe3EsE"
-bot = Bot(token=bot_token)
-dp = Dispatcher()
+# # Define your bot token and create Bot and Dispatcher instances
+# bot_token = "7293607513:AAEMbszahoLrfxC5_28fEojq0AGCkKe3EsE"
+# bot = Bot(token=bot_token)
+# dp = Dispatcher()
 
-# Keyboards
-contact_kb = ReplyKeyboardMarkup(
-    resize_keyboard=True,
-    keyboard=[[KeyboardButton(text="Kontaktni yuborish", request_contact=True)]],
-)
-# Web App Button
-web_app_button = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(
-                text="Bizning sahifamizga utish",
-                web_app=types.WebAppInfo(url="https://www.al-asal.uz/"),
-            )
-        ]
-    ]
-)
-location_kb = ReplyKeyboardMarkup(
-    resize_keyboard=True,
-    keyboard=[[KeyboardButton(text="📍 Lokatsiyani yuborish", request_location=True)]],
-)
-
-
-@dp.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer("hereee")
-
-    photo_url = "https://heartfelt-cascaron-a8b855.netlify.app/assets/logo.jpg"
-    caption_text = (
-        'AsSalomu alaykum! "Al-asal" sahifamizga hush kelibsiz, '
-        "tizimni ishga tushirish uchun kelin tanishib olaylik, uning uchun, "
-        "o`z kontaktingizni junating."
-    )
-    await message.answer_photo(
-        photo=photo_url, caption=caption_text, reply_markup=contact_kb
-    )
+# # Keyboards
+# contact_kb = ReplyKeyboardMarkup(
+#     resize_keyboard=True,
+#     keyboard=[[KeyboardButton(text="Kontaktni yuborish", request_contact=True)]],
+# )
+# # Web App Button
+# web_app_button = InlineKeyboardMarkup(
+#     inline_keyboard=[
+#         [
+#             InlineKeyboardButton(
+#                 text="Bizning sahifamizga utish",
+#                 web_app=types.WebAppInfo(url="https://www.al-asal.uz/"),
+#             )
+#         ]
+#     ]
+# )
+# location_kb = ReplyKeyboardMarkup(
+#     resize_keyboard=True,
+#     keyboard=[[KeyboardButton(text="📍 Lokatsiyani yuborish", request_location=True)]],
+# )
 
 
-@dp.message(ContentTypes.CONTACT)
-async def cmd_contact(message: types.Message):
-    await message.answer(
-        "Bizning sahifamizga utish uchun quyidagi tugmani bosing:",
-        reply_markup=web_app_button,
-    )
-    await message.answer("Iltimos, lokatsiyangizni yuboring:", reply_markup=location_kb)
+# @dp.message(Command("start"))
+# async def start(message: types.Message):
+#     await message.answer("hereee")
+
+#     photo_url = "https://heartfelt-cascaron-a8b855.netlify.app/assets/logo.jpg"
+#     caption_text = (
+#         'AsSalomu alaykum! "Al-asal" sahifamizga hush kelibsiz, '
+#         "tizimni ishga tushirish uchun kelin tanishib olaylik, uning uchun, "
+#         "o`z kontaktingizni junating."
+#     )
+#     await message.answer_photo(
+#         photo=photo_url, caption=caption_text, reply_markup=contact_kb
+#     )
 
 
-@dp.message(ContentTypes.LOCATION)
-async def handle_location(message: types.Message):
-    await message.answer(
-        "Sizning manzilingiz qabul qilindi. Operatorlarimiz 24 soat ichida siz bilan bog'lanadi.",
-        reply_markup=types.ReplyKeyboardRemove(),
-    )
+# @dp.message(F.content_type.in_[ContentType.CONTACT])
+# async def cmd_contact(message: types.Message):
+#     await message.answer(
+#         "Bizning sahifamizga utish uchun quyidagi tugmani bosing:",
+#         reply_markup=web_app_button,
+#     )
+#     await message.answer("Iltimos, lokatsiyangizni yuboring:", reply_markup=location_kb)
 
 
-async def main() -> None:
+# @dp.message(F.content_type.in_[ContentType.LOCATION])
+# async def handle_location(message: types.Message):
+#     await message.answer(
+#         "Sizning manzilingiz qabul qilindi. Operatorlarimiz 24 soat ichida siz bilan bog'lanadi.",
+#         reply_markup=types.ReplyKeyboardRemove(),
+#     )
 
-    await dp.start_polling(bot)
+
+# async def main() -> None:
+
+#     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# if __name__ == "__main__":
+#     asyncio.run(main())
