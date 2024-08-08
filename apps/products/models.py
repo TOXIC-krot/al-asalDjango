c5e6eee7207d816a1964065605f59d9872509199
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-08 16:01:12 +0500

    feat: product models

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse_lazy
 
-# Create your models here.
+from apps.common.models import BaseModel
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name = "Asal turi"
+        verbose_name_plural = "Asal turlari"
+
+    def __str__(self):
+        return self.title
+
+
+class Product(BaseModel):
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.PROTECT
+    )
+
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+
+    image = models.ImageField(upload_to="products/")
+    weight = models.DecimalField(max_digits=10, decimal_places=1)
+
+    price = models.PositiveIntegerField(default=0)
+    count = models.PositiveIntegerField(default=0)
+
+    is_available = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Mahsulot"
+        verbose_name_plural = "Mahsulotlar"
+
+    def __str__(self):
+        return self.title
