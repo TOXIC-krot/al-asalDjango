ffc753820535711a8e6ba52c870f9caafd4399ae
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-08 15:47:54 +0500

    feat: base model

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
