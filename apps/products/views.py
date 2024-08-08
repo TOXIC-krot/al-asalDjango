d0c92c440fba7376c4c5c39dff10aea179ef42ef
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-08 20:57:24 +0500

    fix

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from apps.products import models
+
+
+class ProductListView(generic.ListView):
+    model = models.Product
+    template_name = "products/products_list.html"
+    context_object_name = "products"
