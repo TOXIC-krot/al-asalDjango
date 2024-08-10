36061336aba628b74627a627de8c2fbc5ffa50d4
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-10 20:55:23 +0500

    fix: product list by category view

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -4,6 +4,10 @@ from apps.products import views
 
 urlpatterns = [
     path("", views.ProductListView.as_view(), name="product_list"),
-    path("<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
-    path("category/<int:category_id>/", views.ProductListByCategoryView.as_view(), name="product_list_by_category"),
+    path(
+        "category/<int:category_id>/",
+        views.ProductListView.as_view(),
+        name="product_list_by_category",
+    ),
+    path("<int:pk>", views.ProductDetailView.as_view(), name="product_detail"),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -7,21 +7,17 @@ class ProductListView(generic.ListView):
     template_name = "products/product_list.html"
     context_object_name = "products"
 
+    def get_queryset(self):
+        category_id = self.kwargs.get("category_id")
+        if category_id:
+            return models.Product.objects.filter(category_id=category_id)
+        return models.Product.objects.all()
+
 
 class ProductDetailView(generic.DetailView):
     model = models.Product
     template_name = "products/product_detail.html"
     context_object_name = "product"
-
-
-class ProductListByCategoryView(generic.ListView):
-    model = models.Product
-    template_name = "products/product_list.html"
-    context_object_name = "products"
-    
-    def get_queryset(self):
-        category_id = self.kwargs["category_id"]
-        return models.Product.objects.filter(category_id=category_id)
 
 
 __all__ = (
