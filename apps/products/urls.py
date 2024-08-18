fbea52a8aa6cfe82336ec26679352bb6e2b728bf
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-18 22:29:50 +0500

    fix: url

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -5,7 +5,7 @@ from apps.products import views
 urlpatterns = [
     path("", views.ProductListView.as_view(), name="product_list"),
     path(
-        "<str:telegram_user_token>",
+        "<str:telegram_user_token>/",
         views.ProductListView.as_view(),
         name="product_list",
     ),
