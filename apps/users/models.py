94620aeb6e19ed69359e8c08ba29516d26ee1d28
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-24 10:42:50 +0500

    feat: custom user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from apps.common.models import BaseModel
+from apps.users.managers import UserManager
+
+from core.validators import phone_number_validators
+
+
+class User(AbstractUser, BaseModel):
+    username = None
+    first_name = None
+    last_name = None
+    email = None
+
+    phone_number = models.CharField(
+        max_length=32, validators=phone_number_validators, unique=True
+    )
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "phone_number"
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.phone_number
