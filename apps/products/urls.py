dab0377a06b64e1d0b336f8f4466fa0b179b5d7b
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-24 12:46:26 +0500

    feat: product list

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path
-from apps.products import views
 
+# from apps.products import views
+
+from apps.products.views import ProductListView
 
 urlpatterns = [
-    path("", views.ProductListView.as_view(), name="product_list"),
-    path(
-        "<str:telegram_user_token>/",
-        views.ProductListView.as_view(),
-        name="product_list",
-    ),
-    path(
-        "category/<int:category_id>/",
-        views.ProductListView.as_view(),
-        name="product_list_by_category",
-    ),
-    path("<int:pk>", views.ProductDetailView.as_view(), name="product_detail"),
+    # path("", views.ProductListView.as_view(), name="product_list"),
+    # path(
+    #     "<str:telegram_user_token>/",
+    #     views.ProductListView.as_view(),
+    #     name="product_list",
+    # ),
+    # path(
+    #     "category/<int:category_id>/",
+    #     views.ProductListView.as_view(),
+    #     name="product_list_by_category",
+    # ),
+    # path("<int:pk>", views.ProductDetailView.as_view(), name="product_detail"),
+    path("", ProductListView.as_view(), name="product_list"),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,42 +1,51 @@
-from django.views import generic
-from apps.products import models
-from apps.bot.models import TelegramUser
+# from django.views import generic
+# from apps.products import models
+# from apps.bot.models import TelegramUser
 
 
-class ProductListView(generic.ListView):
-    model = models.Product
-    template_name = "products/product_list.html"
-    context_object_name = "products"
+# class ProductListView(generic.ListView):
+#     model = models.Product
+#     template_name = "products/product_list.html"
+#     context_object_name = "products"
 
-    def get_queryset(self):
-        category_id = self.kwargs.get("category_id")
-        if category_id:
-            return models.Product.objects.filter(
-                category_id=category_id, is_available=True
-            )
-        return models.Product.objects.filter(is_available=True)
+#     def get_queryset(self):
+#         category_id = self.kwargs.get("category_id")
+#         if category_id:
+#             return models.Product.objects.filter(
+#                 category_id=category_id, is_available=True
+#             )
+#         return models.Product.objects.filter(is_available=True)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["categories"] = models.Category.objects.all()
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["categories"] = models.Category.objects.all()
 
-        telegram_user_token = self.kwargs.get("telegram_user_token")
+#         telegram_user_token = self.kwargs.get("telegram_user_token")
 
-        if telegram_user_token:
-            context["telegram_user"] = TelegramUser.objects.get(
-                token=telegram_user_token
-            )
+#         if telegram_user_token:
+#             context["telegram_user"] = TelegramUser.objects.get(
+#                 token=telegram_user_token
+#             )
 
-        return context
-
-
-class ProductDetailView(generic.DetailView):
-    model = models.Product
-    template_name = "products/product_detail.html"
-    context_object_name = "product"
+#         return context
 
 
-__all__ = (
-    "ProductListView",
-    "ProductDetailView",
-)
+# class ProductDetailView(generic.DetailView):
+#     model = models.Product
+#     template_name = "products/product_detail.html"
+#     context_object_name = "product"
+
+
+# __all__ = (
+#     "ProductListView",
+#     "ProductDetailView",
+# )
+
+from rest_framework.generics import ListAPIView
+from apps.products.serializers import ProductListSerializer
+from apps.products.models import Product
+
+
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductListSerializer
