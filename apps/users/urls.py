f2f6ad4b31ed7d131abd7237d5399aeeeb19427d
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-08-25 09:45:26 +0500

    fix: remove verify token

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,7 +12,6 @@ from django.urls import path
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("sign-up/", SignUpView.as_view(), name="sign_up"),
     path("profile/", UserProfileView.as_view(), name="profile"),
 ]
