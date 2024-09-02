b516700591d066ac3a80fde369926592897f3228
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-02 14:15:52 +0500

    feat: register models in admin

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from apps.cart.models import (
+    Cart,
+    CartItem,
+    Order,
+    OrderItem,
+    Saved,
+    SavedItem,
+)
 
-# Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Saved)
+admin.site.register(SavedItem)
