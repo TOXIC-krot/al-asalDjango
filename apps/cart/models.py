4eb2bc5f3f941b3f7a12ee64ccf664202feff56e
Shavkatjon Odamboyev <153182519+Shavkatjon-O@users.noreply.github.com>
2024-09-02 11:58:14 +0500

    feat: cart models

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,13 +1,50 @@
 from django.db import models
 
-'''
-class Cart(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    item = models.CharField(max_length=255)
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    added_at = models.DateTimeField(auto_now_add=True)
+from apps.common.models import BaseModel
+from apps.products.models import Product
+from apps.users.models import User
 
-    def __str__(self):
-        return f"{self.item} - {self.quantity} pcs"
-'''
+
+"""
+OrderStatusChoices
+Order
+OrderItem
+Cart
+CartItem
+"""
+
+
+class OrderStatusChoices(models.TextChoices):
+    PENDING = "PENDING", "Pending"
+    CONFIRMED = "CONFIRMED", "Confirmed"
+    CANCELLED = "CANCELLED", "Cancelled"
+    DELIVERED = "DELIVERED", "Delivered"
+
+
+class Order(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=20,
+        choices=OrderStatusChoices.choices,
+        default=OrderStatusChoices.PENDING,
+    )
+
+
+class OrderItem(BaseModel):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    count = models.PositiveIntegerField(default=1)
+
+
+class Cart(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CartItem(BaseModel):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    count = models.PositiveIntegerField(default=1)
