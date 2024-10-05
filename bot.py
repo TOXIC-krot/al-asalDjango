6d3141da59c340dd31105f93d373423b3049725f
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-10-06 00:20:18 +0500

    fix: bot startup

--- bot.py
+++ bot.py
@@ -11,15 +11,19 @@ from apps.bot.middlewares import AuthMiddleware
 from apps.bot.handlers import router
 
 
+async def on_startup(dispatcher):
+    print("Bot started!")
+
+
 async def main() -> None:
     try:
         dispatcher.include_routers(router)
         dispatcher.message.outer_middleware(AuthMiddleware())
+        dispatcher.startup.register(on_startup)
 
         await bot.delete_webhook(drop_pending_updates=True)
         await dispatcher.start_polling(bot)
 
-        print("Bot started!")
     except KeyboardInterrupt:
         print("Bot stopped manually!")
     except Exception as e:
